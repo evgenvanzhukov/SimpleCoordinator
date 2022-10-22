9ea4e826d3ace286f901af3539a1831c40c496c2
evgen z <evgen-2208@ya.ru>
2022-10-22 13:48:58 +0400

    Initial Commit

---
+++ SimpleCoordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCoordinator
+//
+//  Created by Evgen on 22/10/2022.
+//  Copyright © 2022 Evgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
